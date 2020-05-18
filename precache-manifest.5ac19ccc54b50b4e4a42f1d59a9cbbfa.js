567b23509bfd7b3ab69ecbb312ee87fb28c6b063
Mykhaylo1992 <misha.dyatel@gmail.com>
2020-05-18 15:35:54 +0300

    Updates

---
+++ precache-manifest.5ac19ccc54b50b4e4a42f1d59a9cbbfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31dbcf083aa51bf4f0fcb22c1b863099",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "58846a3062eeca3fbe0e",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.b0d9a0c5.chunk.css"
+  },
+  {
+    "revision": "b6b21d49453147dbc796",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.053596d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.053596d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58846a3062eeca3fbe0e",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.0717c12a.chunk.js"
+  },
+  {
+    "revision": "2c33b5407f6ddb0305a4",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.395ab540.js"
+  }
+]);
